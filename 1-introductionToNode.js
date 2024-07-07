f806a7284d515aaabddc5d2314230fc5c2dedee3
Raees Mughal <raees.mughal.official@gmail.com>
2024-07-07 23:52:54 +0500

    Update 1-introductionToNode.js

--- 1-introductionToNode.js
+++ 1-introductionToNode.js
@@ -3,11 +3,19 @@
 
 
 // how to run javascript
-// 1 : online compiler (provided by w3school);
-// 2: directly in the browser's console
-// 3: in the terminal (command prompt)
+    // 1 : online compiler (provided by w3school);
+    // 2: directly in the browser's console
+    // 3: in the terminal (command prompt)
+        // commands :
+            // just copy paste the initial code and paste in chatGPT prompt
 
-      // commands :
-              // just copy paste the initial code and paste in chatGPT prompt
+    // 4: in vs code terminal
 
-// 4: in vs code terminal
+
+
+
+// basics of javascript
+
+
+
+
