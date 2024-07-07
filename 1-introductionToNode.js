a742f883d3b225de23e163280f73e4705fe1a140
Raees Mughal <raees.mughal.official@gmail.com>
2024-07-07 23:25:13 +0500

    Update 1-introductionToNode.js

--- 1-introductionToNode.js
+++ 1-introductionToNode.js
@@ -6,3 +6,6 @@
 // 1 : online compiler (provided by w3school);
 // 2: directly in the browser's console
 // 3: in the terminal (command prompt)
+
+      // commands :
+              // just copy paste the initial code and paste in chatGPT prompt
